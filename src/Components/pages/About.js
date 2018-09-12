8d4242da3a7b19298be356ebdef4166fb8690608
Carlita Wiggan <info@cwiggan.com>
2018-09-12 09:46:52 -0400

    update about style

--- src/Components/pages/About.js
+++ src/Components/pages/About.js
@@ -7,8 +7,7 @@ export default () => {
             <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
             <hr className="my-4"/>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dicta minus molestiae vel beatae natus eveniet ratione temporibus aperiam harum alias officiis assumenda officia quibusdam deleniti eos cupiditate dolore doloribus!</p>
-
-            <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dicta minus molestiae vel beatae natus eveniet ratione temporibus aperiam harum alias officiis assumenda officia quibusdam deleniti eos cupiditate dolore doloribus!</p>
         </div>
     )
 }
