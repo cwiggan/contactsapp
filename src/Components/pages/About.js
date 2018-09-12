7bdfb55636d57cf6fef4a6b67e633b038e88a813
Carlita Wiggan <info@cwiggan.com>
2018-09-12 09:38:21 -0400

    update about

--- src/Components/pages/About.js
+++ src/Components/pages/About.js
@@ -6,8 +6,9 @@ export default () => {
             <h1 className="display-4">About</h1>
             <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
             <hr className="my-4"/>
-                <p>Version 1</p>
-                <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dicta minus molestiae vel beatae natus eveniet ratione temporibus aperiam harum alias officiis assumenda officia quibusdam deleniti eos cupiditate dolore doloribus!</p>
+
+            <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
         </div>
     )
 }
